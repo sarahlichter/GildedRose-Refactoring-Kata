5dfcb5685ab931d458cccc232c4d9ccd4c8fdf0e
slichter <sarah.lichter>
2020-01-18 19:22:20 -0600

    removed unused functions from GildedRose

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -8,26 +8,7 @@ public class GildedRose {
     public init(items:[Item]) {
         self.items = items
     }
-    
-    fileprivate func decreaseSellIn(_ item: Item) {
-        item.sellIn -= 1
-    }
-    
-    fileprivate func applyBaseQualityAdjustments(_ item: Item) {
-        if item.quality > 0 {
-            item.quality -= 1
-        }
-    }
-    
-    fileprivate func applyQualityAdjustmentsForNegativeSellin(_ item: Item) {
-        if item.sellIn < 0 {
-            if item.quality > 0 {
-                item.quality -= 1
-            }
-
-        }
-    }
-    
+        
     public func adjustValues() {
         for item in items {
             switch item.name {
