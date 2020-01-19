6fc0ce500b992a1bef1097b349085c8f4074881a
slichter <sarah.lichter>
2020-01-18 19:13:56 -0600

    flipped brie logic to make it easier to extract

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -3,6 +3,7 @@ public class GildedRose {
     
     let sulfuras = "Sulfuras, Hand of Ragnaros"
     let backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+    let brie = "Aged Brie"
     
     public init(items:[Item]) {
         self.items = items
@@ -13,26 +14,27 @@ public class GildedRose {
     }
     
     fileprivate func applyBaseQualityAdjustments(_ item: Item) {
-        if item.name != "Aged Brie" {
-            if item.quality > 0 {
-                    item.quality -= 1
-            }
-        } else {
+        if item.name == brie {
             if item.quality < 50 {
                 item.quality += 1
+            }
+        } else {
+            if item.quality > 0 {
+                item.quality -= 1
             }
         }
     }
     
     fileprivate func applyQualityAdjustmentsForNegativeSellin(_ item: Item) {
         if item.sellIn < 0 {
-            if item.name != "Aged Brie" {
-                if item.quality > 0 {
-                    item.quality -= 1
-                }
-            } else {
+            if item.name == brie {
                 if item.quality < 50 {
                     item.quality += 1
+                }
+            } else {
+
+                if item.quality > 0 {
+                    item.quality -= 1
                 }
             }
         }
