85b2f2a0819753c5ee452ab6869c20b42e26c317
slichter <sarah.lichter>
2020-01-18 18:44:49 -0600

    extracted methods out of loop

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -5,56 +5,65 @@ public class GildedRose {
         self.items = items
     }
     
-    public func adjustValues() {
-        for item in items {
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality -= 1
+    fileprivate func decreaseSellIn(_ item: Item) {
+        if item.name != "Sulfuras, Hand of Ragnaros" {
+            item.sellIn -= 1
+        }
+    }
+    
+    fileprivate func applyBaseQualityAdjustments(_ item: Item) {
+        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
+            if item.quality > 0 {
+                if item.name != "Sulfuras, Hand of Ragnaros" {
+                    item.quality -= 1
+                }
+            }
+        } else {
+            if item.quality < 50 {
+                item.quality += 1
+                
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.sellIn < 11 {
+                        if item.quality < 50 {
+                            item.quality += 1
+                        }
                     }
+                    
+                    if item.sellIn < 6 {
+                        if item.quality < 50 {
+                            item.quality += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    fileprivate func applyQualityAdjustmentsForNegativeSellin(_ item: Item) {
+        if item.sellIn < 0 {
+            if item.name != "Aged Brie" {
+                if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.quality > 0 {
+                        if item.name != "Sulfuras, Hand of Ragnaros" {
+                            item.quality -= 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
                 }
             } else {
                 if item.quality < 50 {
                     item.quality += 1
-                    
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sellIn < 11 {
-                            if item.quality < 50 {
-                                item.quality += 1
-                            }
-                        }
-                        
-                        if item.sellIn < 6 {
-                            if item.quality < 50 {
-                                item.quality += 1
-                            }
-                        }
-                    }
                 }
             }
-            
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sellIn -= 1
-            }
-        
-            
-            if item.sellIn < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality -= 1
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality += 1
-                    }
-                }
-            }
+        }
+    }
+    
+    public func adjustValues() {
+        for item in items {
+            applyBaseQualityAdjustments(item)
+            decreaseSellIn(item)
+            applyQualityAdjustmentsForNegativeSellin(item)
 
         }
 
